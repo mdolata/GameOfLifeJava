72d8a5830c9d460ebe50f005677528b12a725d24
mdolata <mateuszdolata2@gmail.com>
2018-09-02 00:57:43 +0200

    add test cases for board

--- src/test/java/board/BoardTest.java
+++ src/test/java/board/BoardTest.java
@@ -1,7 +1,23 @@
 package board;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class BoardTest {
 
+    @Test
+    public void createBoardForFirstRound(){
+
+    }
+
+    @Test
+    public void createBoardForNthRound(){
+
+    }
+
+    @Test
+    public void checkBoardsImmutability(){
+
+    }
 }
