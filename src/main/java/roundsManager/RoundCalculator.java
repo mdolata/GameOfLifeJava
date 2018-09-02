b5cb681527de83b0611fb515a5ceb3b5f565d972
mdolata <mateuszdolata2@gmail.com>
2018-09-02 20:12:40 +0200

    round calculator simply test

--- src/main/java/roundsManager/RoundCalculator.java
+++ src/main/java/roundsManager/RoundCalculator.java
@@ -4,17 +4,31 @@ import board.Board;
 import com.google.common.collect.ImmutableList;
 
 public class RoundCalculator {
+
     public Board calculateNextRound(Board board) {
         Boolean[][] fromList = create2dArrayFromList(board.getBoardList());
-        Boolean[][] resultArray = getEmptyArray(board.getBoardList());
+        Boolean[][] resultArray = calculateVitalityForBoard(board, fromList);
+        return new Board(getListsFromArray(fromList, resultArray), board.getRound() + 1);
+    }
+
+    private Boolean[][] calculateVitalityForBoard(Board board, Boolean[][] fromList) {
+        Boolean[][] resultArray = getEmpty2DArray(fromList.length, fromList[0].length);
 
         for (int i = 0; i < fromList.length; i++) {
             for (int j = 0; j < fromList[i].length; j++) {
                 resultArray[i][j] = checkVitality(fromList, i, j);
             }
         }
+        return resultArray;
+    }
 
-        return new Board(board.getBoardList(), board.getRound() + 1);
+    private ImmutableList<ImmutableList<Boolean>> getListsFromArray(Boolean[][] fromList, Boolean[][] resultArray) {
+        ImmutableList.Builder<ImmutableList<Boolean>> boardBuilder = ImmutableList.builderWithExpectedSize(fromList.length);
+        for (Boolean[] aResultArray : resultArray) {
+            boardBuilder = boardBuilder.add(ImmutableList.copyOf(aResultArray));
+        }
+
+        return boardBuilder.build();
     }
 
     private Boolean checkVitality(Boolean[][] array, int i, int j) {
@@ -43,13 +57,14 @@ public class RoundCalculator {
         return currentCell;
     }
 
+    //TODO neighbor counter
     private int countAliveNeighbor(Boolean[][] array, int i, int j) {
         return 0;
     }
 
     private Boolean[][] create2dArrayFromList(ImmutableList<ImmutableList<Boolean>> boardList) {
         Boolean[][] booleans;
-        booleans = getEmptyArray(boardList);
+        booleans = getEmpty2DArray(boardList.size(), boardList.get(0).size());
 
         for (int i = 0; i < booleans.length; i++) {
             booleans[i] = createArray(boardList.get(i));
@@ -57,8 +72,8 @@ public class RoundCalculator {
         return booleans;
     }
 
-    private Boolean[][] getEmptyArray(ImmutableList<ImmutableList<Boolean>> boardList) {
-        return new Boolean[boardList.size()][boardList.get(0).size()];
+    private Boolean[][] getEmpty2DArray(int size1, int size2) {
+        return new Boolean[size1][size2];
     }
 
     private Boolean[] createArray(ImmutableList<Boolean> list) {
--- src/test/java/roundsManager/RoundCalculatorTest.java
+++ src/test/java/roundsManager/RoundCalculatorTest.java
@@ -1,7 +1,35 @@
 package roundsManager;
 
+import board.Board;
+import board.BoardValidation;
+import board.Utils.BoardsManager;
+import org.junit.Test;
+
+import java.util.Collection;
+
 import static org.junit.Assert.*;
 
 public class RoundCalculatorTest {
 
+    //TODO refactor this test
+    //split to few smaller tests
+    @Test
+    public void calculateBoardIsValidAndHasTheSameSizeAfter(){
+        RoundCalculator roundCalculator = new RoundCalculator();
+        Board boardNo3 = BoardsManager.getBoardNo3();
+        Board receivedBoard = roundCalculator.calculateNextRound(boardNo3);
+
+        assertTrue(BoardValidation.isValid(receivedBoard));
+        assertEquals(boardNo3.getRound() + 1, receivedBoard.getRound());
+        assertEquals(boardNo3.getBoardList().size(), receivedBoard.getBoardList().size());
+
+        int boardNo3InnerListSize = getInnerListSize(boardNo3);
+        int receivedBoardInnerListSize = getInnerListSize(receivedBoard);
+
+        assertEquals(boardNo3InnerListSize, receivedBoardInnerListSize);
+    }
+
+    private int getInnerListSize(Board boardNo3) {
+        return boardNo3.getBoardList().stream().mapToInt(Collection::size).findFirst().orElse(0);
+    }
 }
