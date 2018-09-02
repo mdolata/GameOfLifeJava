db4359e39a40ea3fa98519c1d423dc6bf5d58e72
mdolata <mateuszdolata2@gmail.com>
2018-09-02 20:11:15 +0200

    simplification board validation

--- src/main/java/board/BoardValidation.java
+++ src/main/java/board/BoardValidation.java
@@ -1,29 +1,16 @@
 package board;
 
-import java.util.AbstractCollection;
-import java.util.stream.IntStream;
+import java.util.Collection;
 
 public class BoardValidation {
+    private static final int EXPECTED_DISTINCT_SIZES_COUNT = 1;
+
     public static boolean isValid(Board board) {
-        return getMax(board) == getMin(board);
-    }
-
-    private static int getMin(Board board) {
-        return getIntStream(board)
-                .min()
-                .orElse(0);
-    }
-
-    private static int getMax(Board board) {
-        return getIntStream(board)
-                .max()
-                .orElse(0);
-    }
-
-    private static IntStream getIntStream(Board board) {
-        return board
+        return EXPECTED_DISTINCT_SIZES_COUNT == board
                 .getBoardList()
                 .stream()
-                .mapToInt(AbstractCollection::size);
+                .mapToInt(Collection::size)
+                .distinct()
+                .count();
     }
 }
