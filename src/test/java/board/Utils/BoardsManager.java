f4fe70767ec6920a0fef1f3fa8d26ab38c1b1be6
mdolata <mateuszdolata2@gmail.com>
2018-09-02 16:59:02 +0200

    add test

--- src/test/java/board/Utils/BoardsManager.java
+++ src/test/java/board/Utils/BoardsManager.java
@@ -8,6 +8,11 @@ public class BoardsManager {
     public static final String EMPTY_BOARD = "\n";
     public static final String BOARD_1 = "##\n##\n";
     public static final String BOARD_2 = "# \n# \n";
+    public static final String BOARD_3 =
+            "# # # # \n" +
+            "# # # # \n" +
+            "# # # # \n" +
+            "# # # # \n";
 
     public static Board getEmptyBoard() {
         return new Board(ImmutableList.of(ImmutableList.of()));
@@ -21,5 +26,11 @@ public class BoardsManager {
     public static Board getBoardNo2() {
         ImmutableList<Boolean> booleans = ImmutableList.of(true, false);
         return new Board(ImmutableList.of(booleans, booleans));
+    }
+
+    public static Board getBoardNo3() {
+        Boolean[] booleans1 = {true, false, true, false, true, false, true, false};
+        ImmutableList<Boolean> booleans = ImmutableList.copyOf(booleans1);
+        return new Board(ImmutableList.of(booleans, booleans, booleans, booleans));
     }
 }
